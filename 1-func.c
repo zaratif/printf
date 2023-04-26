df96c908dfaa87b77074cca66cec5afdd7c40c6d
amellaych <Abdorahmane2001@Gmail.com>
2023-04-26 16:09:34 -0700

    task 1

---
+++ 1-func.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	num = 42;
+
+	printf("The number is %d/n", num);
+	printf("The number is %d/n", num);
+
+	return (0);
+}
